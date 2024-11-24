5ab2e4ca66b02c99eb304364ae01d8655dd5a86b
JavierRiesgo <fjriesgosantos@gmail.com>
2024-11-24 22:44:14 +0100

    primer commit

---
+++ app/src/main/java/com/jriesgo/nicestart/Splash.java
@@ -0,0 +1,62 @@
+package com.jriesgo.nicestart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+    private ImageView logo, ola;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        // Llama al método para abrir la aplicación
+        openApp();
+
+        //animacion
+        logo = findViewById(R.id.logosplash);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.translacion);
+
+        logo.startAnimation(myanim);
+
+        //Fondo ola
+        ola = findViewById(R.id.imagenola);
+        /*
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                .transition(DrawableTransitionOptions.withCrossFade(600))
+                .centerCrop()
+                .into(ola);
+        */
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    private void openApp() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }, 5000);
+    }
+}
